1da8da2abf7f98f1b39adcce1b423b93e531190d
Gal Schlezinger <gal@spitfire.co.il>
2020-03-24 16:00:02 +0200

    slight refactor

--- entrypoint.rb
+++ entrypoint.rb
@@ -3,47 +3,49 @@
 require "json"
 require "octokit"
 
+UniqueId = Struct.new(:id) {
+  def as_html_comment
+    "<!-- unique_id: #{id} -->"
+  end
+
+  def in_str?(str)
+    str.include?(as_html_comment)
+  end
+}
+
+def find_pr_number(event)
+  if ENV.fetch("GITHUB_EVENT_NAME") == "pull_request"
+    return event["number"]
+  else
+    pulls = github.pull_requests(repo, state: "open")
+
+    push_head = event["after"]
+    pr_found = pulls.find { |pr| pr["head"]["sha"] == push_head }
+
+    unless pr_found
+      puts "Couldn't find an open pull request for branch with head at #{push_head}."
+      exit(1)
+    end
+    return pr_found["number"]
+  end
+end
+
 json = File.read(ENV.fetch("GITHUB_EVENT_PATH"))
 event = JSON.parse(json)
 
-if ARGV.empty?
-  puts "Missing arguments"
-  exit(1)
-end
-
-github_token, file_path, unique_id = ARGV
+github_token = ARGV.fetch(0) { raise "GitHub token not provided" }
+file_path = ARGV.fetch(1) { raise "File path not provided" }
+unique_id = ARGV[2] && UniqueId.new(ARGV[2])
 
 github = Octokit::Client.new(access_token: github_token)
-
 repo = event["repository"]["full_name"]
-
-if ENV.fetch("GITHUB_EVENT_NAME") == "pull_request"
-  pr_number = event["number"]
-else
-  pulls = github.pull_requests(repo, state: "open")
-
-  push_head = event["after"]
-  pr_found = pulls.find { |pr| pr["head"]["sha"] == push_head }
-
-  unless pr_found
-    puts "Couldn't find an open pull request for branch with head at #{push_head}."
-    exit(1)
-  end
-  pr_number = pr_found["number"]
-end
-unique_id_comment = if unique_id.nil?
-  ""
-else
-  %(<!-- comment_id: #{unique_id} -->)
-end
-
+pr_number = find_pr_number(event)
 message = unique_id_comment + "\n" + File.read(file_path)
 
 coms = github.issue_comments(repo, pr_number)
 duplicate = coms.find { |c|
   c["user"]["login"] == "github-actions[bot]" &&
-    !unique_id.nil? &&
-    c["body"].include?(unique_id_comment)
+    (unique_id&.in_str?(c["body"]) || message == c["body"])
 }
 
 if duplicate
